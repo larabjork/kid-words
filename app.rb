0e58fc1c53a76570f79d0740423f4b873c1bb536
Lara Bjork <lara.m.bjork@gmail.com>
2019-12-20 11:52:08 -0800

    add CRUD functionality to create a new word entry to the list

--- app.rb
+++ app.rb
@@ -4,3 +4,20 @@ require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
 require('./lib/definition')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_word)
+end
+
+post('/words') do
+  term = params[:word_term]
+  word = Word.new(term, nil)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
