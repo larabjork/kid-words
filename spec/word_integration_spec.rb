514e8aeacc60d5d82d8e524df2744ad377e8cd9a
Lara Bjork <lara.m.bjork@gmail.com>
2019-12-20 13:57:12 -0800

    add correction to one integration spec so that it fails differently

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -17,7 +17,7 @@ describe('create a definition path', {:type => :feature}) do
   it('creates a word and then goes to the word page') do
     word = Word.new('Panda', nil).save
     visit("/words/#{word.id}")
-    fill_in('definition_name', :with => 'Really cute black and white animal')
+    fill_in('definition_term', :with => 'Really cute black and white animal')
     click_on('Add definition')
     expect(page).to have_content('Really cute black and white animal')
   end
