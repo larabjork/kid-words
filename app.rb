52c1928a0991e719f60c5053ec136cf542c4a5c5
Lara Bjork <lara.m.bjork@gmail.com>
2019-12-21 14:44:05 -0800

    fix routing issue with update to definition

--- app.rb
+++ app.rb
@@ -90,7 +90,7 @@ end
 patch('/words/:id/definitions/:definition_id') do
   @word = Word.find_word(params[:id].to_i)
   definition = Definition.find_def(params[:definition_id].to_i)
-  definition.update_def(params[:definition], @word.id)
+  definition.update_def(params[:def_entry], @word.id)
   erb(:word)
 end
 
